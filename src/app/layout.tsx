c8eefe24e2aed66df58df3708a316fe705c6d181
arnxldjxy <arnoldsouthammavong@gmail.com>
2025-02-28 08:54:20 -0800

    update gtmId

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,8 +23,9 @@ export default function RootLayout({
     <html lang="en">
       <body>
         {children}
-        <Analytics />
         <GoogleTagManager gtmId="GTM-MF9KCCRR" />
+
+        <Analytics />
       </body>
     </html>
   );
